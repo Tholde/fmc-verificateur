991520592f957de0d5b847a90f32f885cf24d0c3
Tholde <tholde.pathfinder@gmail.com>
2024-08-26 15:23:55 +0300

    first commit

---
+++ treasure/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Users(models.Model):
+    fullname = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    contact = models.CharField(max_length=15)
+    role = models.CharField(max_length=50)  # verificator or treasure
+    image = models.ImageField(upload_to='images/')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)

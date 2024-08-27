ff6aa422092af2439dcc416750b94da1dda899e3
Tholde <tholde.pathfinder@gmail.com>
2024-08-27 12:43:15 +0300

    first commit

--- treasure/urls.py
+++ treasure/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', index, name='index' )
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('service/', service, name='service'),
+    path('team/', team, name='team'),
+    path('why/', why, name='why')
 ]
--- treasure/views.py
+++ treasure/views.py
@@ -6,3 +6,19 @@ def index(request):
     # from django.http import JsonResponse
     # return JsonResponse({"message": "Hello World"})
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def why(request):
+    return render(request, 'why.html')
